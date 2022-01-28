8a57ea148d70294416bb90d625baa4ac5f33c07c
Ryan Herbst <rherbst@slac.stanford.edu>
2022-01-28 15:57:13 -0800

    Add licensing

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
+#-----------------------------------------------------------------------------
+# Title      : Test Scripts
+#-----------------------------------------------------------------------------
+# This file is part of the TID ID Smartsheets software platform. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the TID ID Smartsheets software platform, including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
 
 import tid_ss_lib.secrets
 import tid_ss_lib.navigate
--- tid_ss_lib/navigate.py
+++ tid_ss_lib/navigate.py
@@ -1,3 +1,14 @@
+#-----------------------------------------------------------------------------
+# Title      : Top Level Navigation Budget Sheet
+#-----------------------------------------------------------------------------
+# This file is part of the TID ID Smartsheets software platform. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the TID ID Smartsheets software platform, including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
 
 import smartsheet
 from . import budget_sheet
